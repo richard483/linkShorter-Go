f58e405c85dce8a36fef95d6606e311bdff2c5fb
richard483 <richard.william483@gmail.com>
2022-12-13 05:35:22 +0700

    fix: uncommented godotenv due to error on deploying

--- configs/env.go
+++ configs/env.go
@@ -1,23 +1,26 @@
 package configs
 
 import (
+	"log"
 	"os"
+
+	"github.com/joho/godotenv"
 )
 
 func EnvMongoURI() string {
-	// err := godotenv.Load()
-	// if err != nil {
-	// 	log.Fatal("Error loading .env file")
-	// }
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
 
 	return os.Getenv("MONGODB_URI")
 }
 
 func EnvMongoDB() string {
-	// err := godotenv.Load()
-	// if err != nil {
-	// 	log.Fatal("Error loading .env file")
-	// }
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
 
 	return os.Getenv("DATABASE")
 }
