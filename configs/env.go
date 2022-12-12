5afd35a0b98acb6931e2e49b60f6513df25824c2
richard483 <richard.william483@gmail.com>
2022-12-13 06:03:35 +0700

    fix: changed .env for koyeb

--- configs/env.go
+++ configs/env.go
@@ -1,26 +1,23 @@
 package configs
 
 import (
-	"log"
 	"os"
-
-	"github.com/joho/godotenv"
 )
 
 func EnvMongoURI() string {
-	err := godotenv.Load("ENV")
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
+	// err := godotenv.Load("ENV")
+	// if err != nil {
+	// 	log.Fatal("Error loading .env file")
+	// }
 
 	return os.Getenv("MONGODB_URI")
 }
 
 func EnvMongoDB() string {
-	err := godotenv.Load("ENV")
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
+	// err := godotenv.Load("ENV")
+	// if err != nil {
+	// 	log.Fatal("Error loading .env file")
+	// }
 
 	return os.Getenv("DATABASE")
 }
